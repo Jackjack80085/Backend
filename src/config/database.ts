2cb4844fa0f3db38fe066097b549d13f22215706
Ashith <ashith@Ashiths-MacBook-Air.local>
2026-03-04 17:35:03 +0530

    Add PostgreSQL adapter for Prisma v7 compatibility

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 declare global {
   // eslint-disable-next-line no-var
   var __prisma: PrismaClient | undefined
 }
 
-const clientOptions: any = { log: ['error', 'warn'] }
+const clientOptions: any = { 
+  log: ['error', 'warn'],
+  adapter: new PrismaPg({
+    pool: new Pool({
+      connectionString: process.env.DATABASE_URL,
+    }),
+  }),
+}
 const prisma = global.__prisma ?? new PrismaClient(clientOptions)
 
 if (process.env.NODE_ENV !== 'production') global.__prisma = prisma
